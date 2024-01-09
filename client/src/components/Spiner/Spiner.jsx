1f24627437dbcb2976d5aca0732d00f546ef3a03
Rashidul <aboutrashidul@gmail.com>
2024-01-09 20:51:24 +0600

    create folder structure

---
+++ client/src/components/Spiner/Spiner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spiner = () => {
+  return (
+    <div>
+      <h1>Spiner.js</h1>
+    </div>
+  );
+};
+
+export default Spiner;
---
+++ client/src/components/pagination/Paginations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Paginations = () => {
+  return <div>Paginations.js</div>;
+};
+
+export default Paginations;
