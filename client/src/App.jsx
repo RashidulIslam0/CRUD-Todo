405027697d618299f47e7c4b5e1522c176f0f525
Rashidul <aboutrashidul@gmail.com>
2024-01-09 21:23:07 +0600

    react-route-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,55 @@
+// import { useState } from "react";
+// import "./App.css";
+// import Headers from "./components/Headers/Headers.jsx";
+// import Home from "./pages/Home.jsx";
+// import Register from "./pages/Register.jsx";
+// import Edit from "./pages/Edit.jsx";
+// import Profile from "./pages/Profile.jsx";
+// import { Route, Routes } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <>
+//       <Headers />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/register" element={<Register />} />
+//         <Route path="/edit" element={<Edit />} />
+//         <Route path="/profile" element={<Profile />} />
+//       </Routes>
+
+//       <button className="btn">Hello World</button>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Headers from "./components/Headers/Headers.jsx";
+
+import Home from "./pages/Home/Home.jsx";
+import Register from "./pages/Register/Register.jsx";
+import Edit from "./pages/Edit/Edit.jsx";
+import Profile from "./pages/Profile/Profile.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <button className="btn">Hello World</button>
+      <>
+        <Headers />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/userprofile/:id" element={<Profile />} />
+        </Routes>
+
+        <button className="btn">Hello World</button>
+      </>
     </>
   );
 }
